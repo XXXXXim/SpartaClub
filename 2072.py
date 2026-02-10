a48ff6d3ad8d29495c6eaa237fb8a4139a5174c4
XXXXXim <simhyeck08@gmail.com>
2026-02-11 01:34:23 +0900

    Add files via upload
    2072. 홀수만 더하기

---
+++ 2072.py
@@ -0,0 +1,9 @@
+T = int(input())
+for tc in range(1, T+1):
+    num = map(int, input().split())
+    for i in num:
+        count = 0
+        if i%2 == 1:
+            count += i
+    print(f'#{tc} {total}')
+            
