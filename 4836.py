f65b2e556d02c850c0e59278c7c1da613a6c1a81
XXXXXim <simhyeck08@gmail.com>
2026-02-13 01:18:21 +0900

    Update 4836.py
    10x10 격자판 생성 이후 이해가 가지않음

---
+++ 4836.py
@@ -0,0 +1,21 @@
+T = int(input())
+for tc in range(1, T+1):
+    #이번 tc에서 색칠할 직사각형 개수
+    N = int(input())
+    # ******10x10 격자판 만들기******
+    board = [[0] * 10 for _ in range(10)]
+    #색칠영역을 N번 처리하기
+    for _ in range(N):
+        #수열 입력받아 각 변수에 저장
+        r1, c1, r2, c2, color = map(int, input().split())
+
+        for r in range(r1,r2+1):
+            for c in range(c1, c2+1):
+                board[r][c] += color
+ 
+    cnt = 0
+    for i in range(10):
+        for j in range(10):
+            if board[i][j]==3:
+                cnt+=1
+    print(f'#{tc} {cnt}')
