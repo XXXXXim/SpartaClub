b8d29616979d25df4209737bdd5524ed9dd602df
XXXXXim <simhyeck08@gmail.com>
2026-02-19 01:06:51 +0900

    4836. 색칠하기
    4836. 색칠하기

--- 4836.py
+++ 4836.py
@@ -1,22 +1,19 @@
-T = int(input())
-for tc in range(1, T+1):
-    #이번 tc에서 색칠할 직사각형 개수
-    N = int(input())
-    # ******10x10 격자판 만들기******
-    board = [[0] * 10 for _ in range(10)]
-    #색칠영역을 N번 처리하기
+T= int(input())
+for tc in range(1,T+1):
+    N=int(input())
+    grid = [[0]*10 for _ in range(10)]
+    purple=0
     for _ in range(N):
-        #수열 입력받아 각 변수에 저장
-        r1, c1, r2, c2, color = map(int, input().split())
-
-        for r in range(r1,r2+1):
-            for c in range(c1, c2+1):
-                board[r][c] += color
- 
-    cnt = 0
-    for i in range(10):
-        for j in range(10):
-            if board[i][j]==3:
-                cnt+=1
-    print(f'#{tc} {cnt}')
-
+        r1,c1,r2,c2,color = map(int, input().split())
+        for i in range(r1, r2+1):
+            for j in range(c1, c2+1):
+                if color == 1:
+                    if grid[i][j] == 2:
+                        purple +=1
+                    grid[i][j] = 1
+                elif color == 2:
+                    if grid[i][j] == 1:
+                        purple +=1
+                    grid[i][j] = 2
+    print(f'#{tc} {purple}')
+        
